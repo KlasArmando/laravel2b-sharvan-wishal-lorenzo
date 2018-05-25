cca11a3ba07de46e98faa7a9caaa41d7bb3b80e1
wishalf <wishalfakira@hotail.com>
2018-05-25 13:51:41 +0200

    add test

--- resources/views/yo.blade.php
+++ resources/views/yo.blade.php
@@ -1,9 +1,44 @@
 <!doctype html>
 <html lang=>
-    <head> yo
+<head>
+    <style>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
 
-    </head>
-    <h1>
-        test
-    </h1>
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover:not(.active) {
+            background-color: #111;
+        }
+
+        .active {
+            background-color: #4CAF50;
+        }
+    </style>
+</head>
+<body>
+
+<ul>
+    <li><a class="active" href="#home">Hoopp</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+</ul>
+
+</body>
 </html>
