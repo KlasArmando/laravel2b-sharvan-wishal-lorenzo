d1e45d35e5cfae8ef55e748b4f2200b1b561adf4
wishalf <wishalfakira@hotail.com>
2018-05-25 13:51:41 +0200

    add test

--- resources/views/yo.blade.php
+++ resources/views/yo.blade.php
@@ -34,7 +34,7 @@
 <body>
 
 <ul>
-    <li><a class="active" href="#home">Home</a></li>
+    <li><a class="active" href="#home">Hoopp</a></li>
     <li><a href="#news">News</a></li>
     <li><a href="#contact">Contact</a></li>
     <li><a href="#about">About</a></li>
